06b2de81100aab2f0d0b3d459e79721cc4ed9dc9
刁科瑜 <d19579662108@qq.com>
2025-11-02 11:15:41 +0800

    Practice(2)
    仿写，但是不知道为什么无法运行

---
+++ Practice(2).c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+int main(void)
+{
+	//cmd命令 calc计算器 notepad记事本 mspaint画图板
+	//system("calc");
+	//system可以打开一个外部的应用程序 如果带路径 需要使用\\或/
+	system("C:\\Program Files\\Tencent\\QQNT\\QQ.exe");
+	return 0;
+
+}
