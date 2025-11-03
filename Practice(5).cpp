e4712ff5c3e96de0631c334058691c7a3d695e1c
刁科瑜 <d19579662108@qq.com>
2025-11-04 00:31:00 +0800

    上传跟课小练习
    练习输出常量、格式控制符、换行

---
+++ Practice(5).cpp
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main()
+{
+	//printf("我的朋友的姓名是：%s。性别是：%s。年龄：%d岁。身高：%f厘米。","刁科瑜","女",18,168.5);
+	//return 0;
+	printf("我叫刁科瑜\r\n");
+	printf("我家住在翻斗花园\n");
+	printf("我的邻居是胡图图");
+	return 0;
+}
