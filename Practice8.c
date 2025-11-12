bbbe39cb6bc34fa2154ff9ad6d1cd6d40a607d40
刁科瑜 <d19579662108@qq.com>
2025-11-12 22:01:23 +0800

    修好电脑后第一次上传
    整型、浮点型、有无符号、测量字节

---
+++ Practice8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float a = 3.14F;
+	printf("%.2f\n", a);
+	
+	double b = 3.14159;
+	printf("%lf\n", b);
+	
+	long double c = 3.111111L;
+	printf("%lf\n", c);
+	
+	return 0;
+
+}
